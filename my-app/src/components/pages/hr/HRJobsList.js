e1a6bda07d260c2bde2b4c9fdebfa02395460ef9
Heesang Yoon <85086571+hsyoon900528@users.noreply.github.com>
2021-11-07 13:31:08 +0900

    pagination

--- my-app/src/components/pages/hr/HRJobsList.js
+++ my-app/src/components/pages/hr/HRJobsList.js
@@ -4,12 +4,20 @@ import Header from '../../layout/Header';
 import Footer from '../../layout/Footer';
 import Card from '../../elements/widgets/Home/Card';
 import { useEffect } from 'react';
+import Pagination from '../../../utilities/Pagination';
+import { paginate } from '../../../utilities/paginate';
 
 // Jobs list로 보시면 됩니다.
 export default function HRJobsList() {
 
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [jobpage, setJobpage] = useState({
+    jobdata :[],
+    pageSize : 5,
+    currentPage : 1
+  
+  });
 
   useEffect(() => {
     fetch(`/job-service/${localStorage.getItem('corpNo')}/jobs`)
@@ -23,6 +31,13 @@ export default function HRJobsList() {
       })
   }, []);
 
+  const handlePageChange = (page) => {
+    setJobpage({...jobpage, currentPage: page})
+  };
+
+  const {jobdata,pageSize,currentPage} = jobpage;
+  const pagedJobs = paginate(data,currentPage,pageSize);
+
 
   if (loading) return <div class="spinner-border text-primary" role="status">잠시만 기다려 주세요</div>;
   return (
@@ -35,18 +50,23 @@ export default function HRJobsList() {
             <div className="content-wrapper">
               <div className="row">
                 {
-                  data.length > 0 && data.map(
-                    (item, idx) => (
+                  pagedJobs.map(
+                    (job,idx)=>(
                       <Card
-                        idx={idx + 1}
-                        key={item.idx}
-                        data={item}
-                        setData={setData}
-                      />
+                        idx = {idx +1}
+                        key = {job.idx}
+                        data = {job} 
+                        setData = {job}/>
                     )
                   )
                 }
               </div>
+              <Pagination
+                itemsCount = {data.length}
+                pageSize = {pageSize}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+              />
             </div>
             <Footer />
           </div>
