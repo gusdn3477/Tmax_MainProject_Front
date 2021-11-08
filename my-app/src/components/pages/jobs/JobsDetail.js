4f88f7d1e843148281c70892244ee8a7886515e7
haribowoo <85086591+hariBoWoo@users.noreply.github.com>
2021-11-08 22:21:21 +0900

    꾸미기 본격적 1차
    가즈아

--- my-app/src/components/pages/jobs/JobsDetail.js
+++ my-app/src/components/pages/jobs/JobsDetail.js
@@ -34,7 +34,7 @@ export default function JobsDetail() {
       );
   }, []);
 
-  if (loading) return <div class="spinner-border text-primary" role="status">잠시만 기다려 주세요</div>;
+  if (loading) return <div class="spinner-border text-primary" role="status"></div>;
   return (
     <div id="wrap">
       <Header />
