55ae4b07dfe188df7f512b84e452a7f7e79b60cc
haribowoo <85086591+hariBoWoo@users.noreply.github.com>
2021-11-09 17:18:09 +0900

    본격적으로 꾸미기 3차
    공고 상세화면

--- my-app/src/components/elements/widgets/Form/JobsDetailForm.js
+++ my-app/src/components/elements/widgets/Form/JobsDetailForm.js
@@ -83,6 +83,10 @@ export default function JobsDetailForm({ data, data2 }) {
   );
 
   return (
+  <div>
+  <link rel="preconnect" href="https://fonts.googleapis.com"/>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500;700;900&display=swap" rel="stylesheet"></link>
     <div className="container-scroller">
       <div className="container-fluid page-body-wrapper full-page-wrapper">
         <div className="content-wrapper d-flex align-items-center auth px-0">
@@ -90,17 +94,20 @@ export default function JobsDetailForm({ data, data2 }) {
             <div className="col-lg-8 mx-auto">
               <div className="auth-form-light text-left py-5 px-4 px-sm-5">
                 <Brand />
-                <h3>{data?.jobsTitle}</h3>
-                <form className="pt-3">
-                  <div className="form-group" style={{ marginBottom: "2rem" }}>
+                <h2 style={{fontFamily:"Noto Snas KR", fontWeight:"900", paddingLeft:"2%", lineHeight:"1.4"}}>{data?.jobsTitle}</h2>
+                
+                <form className="pt-5">
+                  
+                  <div className="form-group display-5" style={{ marginBottom: "4rem"}}>
                     {data?.jobsContext}
+                  </div>{/*
+                  <div className="form-group">
+                    채용 구분 : {data?.employType}
                   </div>
                   <div className="form-group">
-                    채용 형태 : {data?.employType}
-                  </div>
-                  <div className="form-group">고용 형태 : {data?.jobType}</div>
+                    근무 형태 : {data?.jobType}</div>
                   <div className="form-group">
-                    지원 자격 : {data?.jobQualify}
+                    자격 요건 : {data?.jobQualify}
                   </div>
                   <div className="form-group">
                     채용인원 : {data?.recruitNum}명
@@ -121,29 +128,84 @@ export default function JobsDetailForm({ data, data2 }) {
                     </div>
                   ) : (
                     ""
-                  )}
+                  )} */}
 
-                  <div style={{ margin: "5rem 0 2rem 0" }}>
+                  <div className="table-responsive">
+                    <table className="simple_table">
+                      <thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row" >채용구분  </th>
+                            <td>{data?.employType}</td>
+                          </tr>
+                          <tr>
+                            <th>근무형태</th>
+                            <td>{data?.jobType}</td>
+                          </tr>
+                          
+                          {data?.applyStart && data?.applyEnd ? (
+                            <tr>
+                              <th> 지원기간  </th>
+                              <td>{(data?.applyStart).substr(0, 10)} ~{" "}
+                                {(data?.applyEnd).substr(0, 10)}
+                              </td>
+                            </tr>
+                          ) : (
+                            ""
+                          )}
+                          <tr>
+                            <th>우대언어</th>
+                            <td>{data?.favoriteLang}</td>
+                          </tr> 
+                          <tr>
+                            <th>담당업무</th>
+                            <td>{data?.workDetail}</td>
+                          </tr> 
+                          <tr>
+                            <th>근무지역</th>
+                            <td>{data?.jobLocation}</td>
+                          </tr>
+                          <tr>
+                            <th>채용인원</th>
+                            <td>{data?.recruitNum} 명</td>
+                          </tr>
+                          <tr>
+                            <th>자격요건</th>
+                            <td>{data?.jobQualify}</td>
+                          </tr>
+                        </tbody>
+                      </thead>
+                    </table>
+                  </div>
+
+
+
+
+
+
+                  <div class="row align-items-center" style={{ margin: "3rem 10% 2rem " }}>
                     {localStorage.getItem("empNo") ? (
                       <Fragment>
-                        <div className="mt-3">
+                        <div className="mt-3 col-auto w-50" >
                           <button
-                            type="button"
-                            className="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn"
+                            type="button" 
+                            className="btn btn-block btn-inverse-primary btn-icon-text btn-lg font-weight-medium auth-form-btn"
                             onClick={confirmEnd}
                           >
+                            <i className="fas fa-file btn-icon-prepend"></i>
                             마감하기
                           </button>
                         </div>
                         {/* <div className="mt-3">
                       <button type="button" className="btn btn-block btn-warning btn-lg font-weight-medium auth-form-btn">수정하기</button>
                     </div> */}
-                        <div className="mt-3">
+                        <div className="mt-3 col-auto w-50  ">
                           <button
                             type="button"
-                            className="btn btn-block btn-danger btn-lg font-weight-medium auth-form-btn"
+                            className="btn btn-block btn-inverse-danger btn-icon-text btn-lg font-weight-medium auth-form-btn"
                             onClick={confirmDelete}                          
                           >
+                            <i className="fas fa-trash btn-icon-prepend"></i>
                             삭제하기
                           </button>
                         </div>
@@ -154,7 +216,7 @@ export default function JobsDetailForm({ data, data2 }) {
 
                     {localStorage.getItem("userId") ? (
                       <Fragment>
-                        <div className="mt-3">
+                        <div className="mt-3 col-auto">
                           <Link to={`/users/apply/${jobsNo}`}>
                             <button
                               type="button"
@@ -175,6 +237,6 @@ export default function JobsDetailForm({ data, data2 }) {
           </div>
         </div>
       </div>
-    </div>
+    </div></div>
   );
 }
