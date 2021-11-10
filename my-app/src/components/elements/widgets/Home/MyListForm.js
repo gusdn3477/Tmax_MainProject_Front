6c61bcda472bebd7de21ea744a13cf7be19d35a3
박현우(Park Hyun Woo) <46596758+gusdn3477@users.noreply.github.com>
2021-11-10 19:16:16 +0900

    id 속성 다르게 구현

--- my-app/src/components/elements/widgets/Home/MyListForm.js
+++ my-app/src/components/elements/widgets/Home/MyListForm.js
@@ -1,43 +1,39 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import MyApplyListForm from "./MyApplyListForm";
 
 export default function MyListForm({ idx, key, data }) {
-  const [values, setValues] = useState([]);
+  const [written, Setwritten] = useState([]);
   const [interview, setInterview] = useState([]);
   const [loading, setLoading] = useState(true);
   const [applyInfo, setApplyInfo] = useState([]);
 
-  // useEffect(() => {
-  //   fetch(
-  //     `/process-service/process/written/${data.jobsNo}/${localStorage.getItem("userId")}`
-  //   )
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((data) => {
-  //       console.log(data);
-  //       setValues(data);
-  //     })
-  //     .then(
-  //       fetch(
-  //         `/process-service/process/interview/${data.jobsNo
-  //         }/${localStorage.getItem("userId")}`
-  //       )
-  //         .then((res) => {
-  //           return res.json();
-  //         })
-  //         .then((data) => {
-  //           console.log(data);
-  //           setInterview(data);
-  //           setLoading(false);
-  //         })
-  //     );
-  // }, []);
-
   useEffect(() => {
-    fetch(`/user-service/users/apply/${localStorage.getItem("userId")}/${data.jobsNo}`
+    fetch(
+      `/process-service/process/written/${data.jobsNo}/${localStorage.getItem("userId")}`
     )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log('필기 결과', data);
+        Setwritten(data);
+        return data;
+      })
+      .then(
+        res => {
+          fetch(`/process-service/process/interview/${data.jobsNo}/${localStorage.getItem("userId")}`)
+            .then((res) => 
+              res.json()
+            )
+            .then((data) => {
+              console.log('면접 결과', data);
+              setInterview(data);
+            })
+        }
+      );
+
+    // 지원내역 가져오는 부분
+    fetch(`/user-service/users/apply/${localStorage.getItem("userId")}/${data.jobsNo}`)
       .then((res) => {
         return res.json();
       })
@@ -48,13 +44,30 @@ export default function MyListForm({ idx, key, data }) {
       });
   }, []);
 
+  if(!loading){
+    function findResult(){
+      if(written.writtenResult === 'F'){
+        return "아쉽게도 필기전형에서 불합격 하셨습니다.";
+      }
+      else{
+        if(!interview){
+          return "필기 전형 합격입니다.";
+        }
+        else if(interview.firstInterviewResult === 'F'){
+          return "1차 면접에서 불합격 하셨습니다.";
+        }
+      }
+    }
+  }
+
   // 이걸로 결과 얻기
   if (loading)
     return (
-      <div class="spinner-border text-primary" role="status">
+      <div className="spinner-border text-primary" role="status">
         잠시만 기다려 주세요
       </div>
     );
+
   return (
     <tr>
       <td>{idx}</td>
@@ -69,53 +82,51 @@ export default function MyListForm({ idx, key, data }) {
       <td>{data.jobLocation}</td>
       <td>
         {/* <MyApplyListForm data2={data} /> */}
-
         <div>
           <button
             type="button"
-            class="btn btn-primary"
+            className="btn btn-primary"
             data-bs-toggle="modal"
-            data-bs-target={`#exampleModal`+idx}
+            data-bs-target={`#exampleModal` + idx}
           >
             내 지원서 보기
           </button>
         </div>
         <div
-          class="modal fade"
-          id={`exampleModal`+idx}
+          className="modal fade"
+          id={`exampleModal` + idx}
           tabindex="-1"
           aria-labelledby="exampleModalLabel"
           aria-hidden="true"
         >
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
                   내 지원서
                 </h5>
                 <button
                   type="button"
-                  class="btn-close"
+                  className="btn-close"
                   data-bs-dismiss="modal"
                   aria-label="Close"
                 ></button>
               </div>
-              <div class="modal-body" value={data}>
+              <div className="modal-body" value={data}>
                 <h3>최종 지원이 완료되었습니다.</h3>
                 <br></br> 이름 : {applyInfo.applyName} <br></br>
                 <br></br> 이메일 : {applyInfo.applyEmail} <br></br>
                 <br></br> 전화번호 : {applyInfo.applyContact} <br></br>
-                <br></br> 지원날짜 : {(applyInfo.applyDateTime).substring(0,10)} <br></br>
+                <br></br> 지원날짜 : {(applyInfo.applyDateTime).substring(0, 10)} <br></br>
                 {/* <br></br> data.jobsNo : {data.jobsNo} <br></br> */}
                 <br></br> 지원번호 : {applyInfo.jobsNo} <br></br>
               </div>
               {/* <div>{applyList}</div> */}
-              <div class="modal-footer">
+              <div className="modal-footer">
                 <button
                   type="button"
-                  class="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                >
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal">
                   닫기
                 </button>
               </div>
@@ -124,13 +135,8 @@ export default function MyListForm({ idx, key, data }) {
         </div>
       </td>
       <td>
-        {values.writtenResult ? (
-          <button
-            type="button"
-            class="btn btn-primary"
-            data-bs-toggle="modal"
-            data-bs-target="#exampleModal2"
-          >
+        {written.writtenResult ? (
+          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#exampleModalTwo`+idx}>
             확인하기
           </button>
         ) : (
@@ -139,39 +145,42 @@ export default function MyListForm({ idx, key, data }) {
       </td>
 
       {/* 잠시 대기 */}
-      {/* <div
-        class="modal fade"
-        id="exampleModal2"
+      <div
+        className="modal fade"
+        id={`exampleModalTwo`+idx}
         tabindex="-1"
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
       >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
                 지원 결과
               </h5>
               <button
                 type="button"
-                class="btn-close"
+                className="btn-close"
                 data-bs-dismiss="modal"
                 aria-label="Close"
               ></button>
             </div>
-            <div class="modal-body">조만간 구현할 예정입니다.</div>
-            <div class="modal-footer">
+            <div className="modal-body">
+              <findResult/>
+              {written.writtenResult === 'P' ? "1차 합격" : "대기"}
+              
+              </div>
+            <div className="modal-footer">
               <button
                 type="button"
-                class="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
+                className="btn btn-secondary"
+                data-bs-dismiss="modal">
                 닫기
               </button>
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
     </tr>
   );
 }
