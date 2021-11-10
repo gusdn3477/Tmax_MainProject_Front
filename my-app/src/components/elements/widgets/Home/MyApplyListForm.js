df3f9e5655bf578e74cba9ac78dae8f22dd259db
KIM YOUNG MO <yiyi6464@naver.com>
2021-11-10 15:19:45 +0900

    Abc

--- my-app/src/components/elements/widgets/Home/MyApplyListForm.js
+++ my-app/src/components/elements/widgets/Home/MyApplyListForm.js
@@ -1,22 +1,40 @@
+import { data } from "jquery";
 import { useState, useEffect } from "react";
 
 export default function MyApplyListForm({ data2 }) {
-  const [applyInfo, setApplyInfo] = useState([]);
+  const [applyInfo, setApplyInfo] = useState([data2]);
 
   console.log(data2); //jobsNo
 
+  console.log(applyInfo.jobsNo);
+  const [applyJobsNo, setApplyJobsNo] = useState([data2]);
+
+  console.log("applyInfo :" + data2.jobsNo);
+
   useEffect(() => {
-    fetch(`/user-service/users/${localStorage.getItem("userId")}`)
+    fetch(
+      `/user-service/users/apply/${localStorage.getItem("userId")}/${
+        data2.jobsNo
+      }`
+    )
       .then((res) => {
         return res.json();
       })
-      .then((data) => {
-        console.log(data);
-        setApplyInfo(data);
+      .then((event) => {
+        console.log(event);
+        setApplyInfo(event);
+        console.log(applyInfo);
       });
   }, []);
 
+  console.log(applyInfo);
+
+  function gogo(data2) {}
+
   return (
+    // <div>
+    //   <button onClick={gogo(data2)}>내 지원서 보기기기기</button>
+    // </div>
     <div>
       <div>
         <button
@@ -25,7 +43,7 @@ export default function MyApplyListForm({ data2 }) {
           data-bs-toggle="modal"
           data-bs-target="#exampleModal"
         >
-          내 지원서 보기
+          내 지원서 보기 {data2.jobsNo}
         </button>
       </div>
       <div
@@ -50,8 +68,13 @@ export default function MyApplyListForm({ data2 }) {
             </div>
             <div class="modal-body">
               <h3>최종 지원이 완료되었습니다.</h3>
-              {applyInfo.email}
-              {applyInfo.name}
+              <br></br> 이름 : {applyInfo.applyName} <br></br>
+              <br></br> 이메일 : {applyInfo.applyEmail} <br></br>
+              <br></br> 전화번호 : {applyInfo.applyContact} <br></br>
+              <br></br> 지원날짜 : {applyInfo.applyDateTime} <br></br>
+              <br></br> data.jobsNo : {data2.jobsNo} <br></br>
+              <br></br> applyInfo.jobsNo : {applyInfo.jobsNo} <br></br>
+              <br></br> value.jobsNo : $value <br></br>
             </div>
             <div class="modal-footer">
               <button
