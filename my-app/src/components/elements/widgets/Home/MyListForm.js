3b4011f7f59ba2b9b54c1c4f53e9dd66a904de36
KIM YOUNG MO <yiyi6464@naver.com>
2021-11-11 16:56:34 +0900

    abcadf

--- my-app/src/components/elements/widgets/Home/MyListForm.js
+++ my-app/src/components/elements/widgets/Home/MyListForm.js
@@ -7,53 +7,84 @@ export default function MyListForm({ idx, key, data }) {
   const [loading, setLoading] = useState(true);
   const [applyInfo, setApplyInfo] = useState([]);
 
+  // useEffect(() => {
+  //   fetch(
+  //     `/process-service/process/written/${data.jobsNo}/${localStorage.getItem(
+  //       "userId"
+  //     )}`
+  //   )
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       console.log("필기 결과", data);
+  //       Setwritten(data);
+  //       return data;
+  //     })
+  //     .then((res) => {
+  //       fetch(
+  //         `/process-service/process/interview/${
+  //           data.jobsNo
+  //         }/${localStorage.getItem("userId")}`
+  //       )
+  //         .then((res) => res.json())
+  //         .then((data) => {
+  //           console.log("면접 결과", data);
+  //           setInterview(data);
+  //         });
+  //     });
+
   useEffect(() => {
     fetch(
-      `/process-service/process/written/${data.jobsNo}/${localStorage.getItem("userId")}`
+      `/process-service/process/written/${data.jobsNo}/${localStorage.getItem(
+        "userId"
+      )}`
     )
       .then((res) => {
         return res.json();
       })
       .then((data) => {
-        console.log('필기 결과', data);
+        console.log("필기 결과", data);
         Setwritten(data);
         return data;
       })
-      .then(
-        res => {
-          fetch(`/process-service/process/interview/${data.jobsNo}/${localStorage.getItem("userId")}`)
-            .then((res) => 
-              res.json()
-            )
-            .then((data) => {
-              console.log('면접 결과', data);
-              setInterview(data);
-            })
-        }
-      );
+      .then((res) => {});
+
+    fetch(
+      `/process-service/process/interview/${data.jobsNo}/${localStorage.getItem(
+        "userId"
+      )}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("면접 결과", data);
+        setInterview(data);
+      });
 
     // 지원내역 가져오는 부분
-    fetch(`/user-service/users/apply/${localStorage.getItem("userId")}/${data.jobsNo}`)
+    fetch(
+      `/user-service/users/apply/${localStorage.getItem("userId")}/${
+        data.jobsNo
+      }`
+    )
       .then((res) => {
         return res.json();
       })
-      .then(res => {
+      .then((res) => {
         setApplyInfo(res);
-        console.log('data', applyInfo);
+        console.log("data", applyInfo);
         setLoading(false);
       });
   }, []);
 
-  if(!loading){
-    function findResult(){
-      if(written.writtenResult === 'F'){
+  if (!loading) {
+    function findResult() {
+      if (written.writtenResult === "F") {
         return "아쉽게도 필기전형에서 불합격 하셨습니다.";
-      }
-      else{
-        if(!interview){
+      } else {
+        if (!interview) {
           return "필기 전형 합격입니다.";
-        }
-        else if(interview.firstInterviewResult === 'F'){
+        } else if (interview.firstInterviewResult === "F") {
           return "1차 면접에서 불합격 하셨습니다.";
         }
       }
@@ -117,7 +148,8 @@ export default function MyListForm({ idx, key, data }) {
                 <br></br> 이름 : {applyInfo.applyName} <br></br>
                 <br></br> 이메일 : {applyInfo.applyEmail} <br></br>
                 <br></br> 전화번호 : {applyInfo.applyContact} <br></br>
-                <br></br> 지원날짜 : {(applyInfo.applyDateTime).substring(0, 10)} <br></br>
+                <br></br> 지원날짜 : {applyInfo.applyDateTime.substring(0, 10)}{" "}
+                <br></br>
                 {/* <br></br> data.jobsNo : {data.jobsNo} <br></br> */}
                 <br></br> 지원번호 : {applyInfo.jobsNo} <br></br>
               </div>
@@ -126,7 +158,8 @@ export default function MyListForm({ idx, key, data }) {
                 <button
                   type="button"
                   className="btn btn-secondary"
-                  data-bs-dismiss="modal">
+                  data-bs-dismiss="modal"
+                >
                   닫기
                 </button>
               </div>
@@ -135,19 +168,33 @@ export default function MyListForm({ idx, key, data }) {
         </div>
       </td>
       <td>
-        {written.writtenResult ? (
-          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#exampleModalTwo`+idx}>
+        {/* {written.writtenResult ? (
+          <button
+            type="button"
+            className="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target={`#exampleModalTwo` + idx}
+          >
             확인하기
           </button>
         ) : (
           "미정"
-        )}
+        )} */}
+
+        <button
+          type="button"
+          className="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target={`#exampleModalTwo` + idx}
+        >
+          확인하기
+        </button>
       </td>
 
       {/* 잠시 대기 */}
       <div
         className="modal fade"
-        id={`exampleModalTwo`+idx}
+        id={`exampleModalTwo` + idx}
         tabindex="-1"
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
@@ -166,15 +213,29 @@ export default function MyListForm({ idx, key, data }) {
               ></button>
             </div>
             <div className="modal-body">
-              <findResult/>
-              {written.writtenResult === 'P' ? "1차 합격" : "대기"}
-              
-              </div>
+              {/* <findResult />
+              {written.writtenResult === "P" ? "1차 합격" : "대기"}
+              {interview.firstInterviewResult === "p"
+                ? "1차면접 합겨억"
+                : "대기"} */}
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>필기 전형 결과 : {written.writtenResult}
+              <br></br>
+              <br></br>첫번째 면접 결과 : {interview.firstInterviewResult}
+              <br></br>
+              <br></br>두번째 면접 결과 : {interview.secondInterviewResult}
+              <br></br>
+            </div>
             <div className="modal-footer">
               <button
                 type="button"
                 className="btn btn-secondary"
-                data-bs-dismiss="modal">
+                data-bs-dismiss="modal"
+              >
                 닫기
               </button>
             </div>
